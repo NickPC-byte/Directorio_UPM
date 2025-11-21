efd3ff6db35105eb68f3c9caa3a845687552a955
NickPC-byte <lucio.palomino@unh.edu.pe>
2025-11-21 12:40:12 -0500

    Create data.js

---
+++ data.js
@@ -0,0 +1,42 @@
+const directorio = [
+  {
+    nombre: "Katerin Rosa Contreras Taipe",
+    celular: "952662622",
+    correo: "katerin.contreras@unh.edu.pe"
+  },
+  {
+    nombre: "Jaime Castro Contreras",
+    celular: "938751671",
+    correo: "jaime.castro@unh.edu.pe"
+  },
+  {
+    nombre: "Jerson Brayan Lucas Ramos",
+    celular: "966708747",
+    correo: "jerson.lucas@unh.edu.pe"
+  },
+  {
+    nombre: "Jesús Gustavo Gavilán Castillo",
+    celular: "922673683",
+    correo: "jesus.gavilan@unh.edu.pe"
+  },
+  {
+    nombre: "Karen Yasmín Canales Lucas",
+    celular: "901914267",
+    correo: "karen.canales@unh.edu.pe"
+  },
+  {
+    nombre: "Lucio Nicolay Palomino Calderón",
+    celular: "982322159",
+    correo: "lucio.palomino@unh.edu.pe"
+  },
+  {
+    nombre: "Ronal Morán Trillo",
+    celular: "921919458",
+    correo: "ronald.moran@unh.edu.pe"
+  },
+  {
+    nombre: "Sandra Cahuana Huamaní",
+    celular: "968496494",
+    correo: "sandra.cahuana@unh.edu.pe"
+  }
+];
